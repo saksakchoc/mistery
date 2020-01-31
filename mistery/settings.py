29ae627a54325f8d67438c59e842a6ef8d62657f
aiko <saksak.ch@gmail.com>
2020-01-31 22:32:28 +0900

    for lolipop

--- mistery/settings.py
+++ mistery/settings.py
@@ -11,7 +11,11 @@ https://docs.djangoproject.com/en/3.0/ref/settings/
 """
 
 import os
-
+import socket
+if socket.get_hostname() == 'joe-mac-mini':
+    #Development Server Settings go here
+else:
+    #Production Server Settings go here
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
@@ -119,3 +123,6 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
 STATIC_URL = '/static/'
+ADMINS = (
+    ('aiko', 'saksak.ch@gmail.com'),
+)
