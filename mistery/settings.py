4cf21c46786a2ff581c84285b71c047fdd34abc3
saksakchoc <saksak.ch@gmail.com>
2020-02-01 11:28:52 +0900

    for modal

--- mistery/settings.py
+++ mistery/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'imu2+!em0xvg(st1l@kr_o^kcpkh_bt*+%9&i+3m90qtcg=eh2'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
